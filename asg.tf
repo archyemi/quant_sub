68120012530179d867c0e3d71441b19aa8dca771
Adeyemi Alatishe <36404707+archyemi@users.noreply.github.com>
2023-10-29 09:48:10 -0700

    Update asg.tf

--- asg.tf
+++ asg.tf
@@ -5,7 +5,7 @@ resource "aws_autoscaling_group" "submission-autoscaling" {
     id      = aws_launch_template.submission_lt.id
     version = "$Latest"
   }
-  min_size                  = 1
+  min_size                  = 2
   max_size                  = 2
   health_check_grace_period = 300
   health_check_type         = "ELB"
